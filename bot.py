58b46c7c69ff73f2bcf324968870a4a92ee438ea
Admin <admin@example.com>
2026-08-11 15:42:09 +0300

    commit

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 import asyncio
 import logging
 
-# В самом начале bot.py, после импортов
-import os
-print(f"🔑 Токен из окружения: {os.getenv('BOT_TOKEN')}")
-print(f"🔑 Токен из config: {BOT_TOKEN}")
-
 from aiogram import Bot, Dispatcher, types  # Dispatcher импортирован здесь
 from aiogram.types import BotCommand
-
 from config import BOT_TOKEN
 from loader import dp, bot
 from handlers import user, admin  # Импортируем обработчики для их регистрации
